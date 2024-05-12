00e493cd0cb4c5108db4dab71c893f5bd3f2d900
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-05-12 18:22:28 -0300

    Converting indentation to tabs

--- tape.py
+++ tape.py
@@ -3,55 +3,55 @@ DEFAULT_SIZE: int = 30_000
 
 class Tape(list[int]):
 
-    def __init__(self, size: int = DEFAULT_SIZE) -> None:
-        super().__init__([0] * size)
+	def __init__(self, size: int = DEFAULT_SIZE) -> None:
+		super().__init__([0] * size)
 
-        self.__pointer: int = 0
-
-
-    @property
-    def pointer(self) -> int:
-        return self.__pointer
+		self.__pointer: int = 0
 
 
-    @pointer.setter
-    def pointer(self, value: int) -> None:
-        
-        if value < 0:
-            raise ValueError(f"New pointer value is negative; {value} < 0.")
-
-        self.__pointer = value
+	@property
+	def pointer(self) -> int:
+		return self.__pointer
 
 
-    @property
-    def byte(self) -> int:
-        return self[self.pointer]
-    
+	@pointer.setter
+	def pointer(self, value: int) -> None:
+		
+		if value < 0:
+			raise ValueError(f"New pointer value is negative; {value} < 0.")
 
-    @byte.setter
-    def byte(self, value: int) -> None:
-        self[self.pointer] = value & 0xff
+		self.__pointer = value
 
 
-    def shift_by(self, ammount: int) -> None:
-        self.pointer += ammount
-    
+	@property
+	def byte(self) -> int:
+		return self[self.pointer]
+	
 
-    def shift_right(self) -> None:
-        self.shift_by(1)
-    
+	@byte.setter
+	def byte(self, value: int) -> None:
+		self[self.pointer] = value & 0xff
 
-    def shift_left(self) -> None:
-        self.shift_by(-1)
-    
 
-    def increase_by(self, ammount: int) -> None:
-        self.byte += ammount
-    
+	def shift_by(self, ammount: int) -> None:
+		self.pointer += ammount
+	
 
-    def increase(self) -> None:
-        self.increase_by(1)
-    
+	def shift_right(self) -> None:
+		self.shift_by(1)
+	
 
-    def decrease(self) -> None:
-        self.increase_by(-1)
+	def shift_left(self) -> None:
+		self.shift_by(-1)
+	
+
+	def increase_by(self, ammount: int) -> None:
+		self.byte += ammount
+	
+
+	def increase(self) -> None:
+		self.increase_by(1)
+	
+
+	def decrease(self) -> None:
+		self.increase_by(-1)

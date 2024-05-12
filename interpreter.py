78b7f9a9c5324c4c1c5a50ba475844b2010a400c
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-05-12 18:36:47 -0300

    Adding function for while map

--- interpreter.py
+++ interpreter.py
@@ -8,10 +8,10 @@ class Interpreter:
 
 	def __init__(self, code: str) -> None:
 		
+		self.tape: Tape = Tape()
+
 		self.code: str = code
 		self.filtered: list[int] = [TOKEN_MAP[i] for i in self.code if i in TOKEN_MAP]
-
-		self.tape: Tape = Tape()
 
 		self.converter: list[Callable[[], None]] = [
 			self.tape.shift_right,
@@ -26,6 +26,25 @@ class Interpreter:
 		self.__input: list[int] = []
 
 
+	def __get_map(self) -> dict[int, int]:
+
+		queue: list[int] = []
+		result: dict[int, int] = {}
+
+		__open_brace: int = TOKEN_MAP['[']
+		__close_brace: int = TOKEN_MAP[']']
+
+		for i, a in enumerate(self.filtered):
+			
+			if a == __open_brace:
+				queue.append(i)
+			
+			elif a == __close_brace:
+				result[i] = queue.pop()
+				
+		return result
+
+
 	def output(self) -> None:
 		self.__output += chr(self.tape.byte)
 	
@@ -33,3 +52,5 @@ class Interpreter:
 	def read(self) -> None:
 		self.tape.byte = self.__input.pop()
 	
+
+

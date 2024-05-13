6520e5cef29a16374060eb8273809f0ad6d3373e
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-05-12 23:05:35 -0300

    Adding centering by index

--- text.py
+++ text.py
@@ -1,17 +1,20 @@
 
 import pygame as pg
 
-FontInfo = tuple[str, int, bool, bool]
+pg.init()
 
-__saved_fonts: dict[FontInfo, pg.font.Font]
+FontInfo = tuple[str, int, bool, bool]
 
 class Text:
 
 	string: str
 	font: pg.font.Font
+	color: tuple[int, int, int]
 
 	surface: pg.Surface
 	rect: pg.Rect
+	
+	__saved_fonts: dict[FontInfo, pg.font.Font] = {}
 
 	@staticmethod
 	def get_font(name: str = "ヒラキノ角コシックw0", size: int = 30, 
@@ -19,12 +22,12 @@ class Text:
 
 		info: FontInfo = (name, size, bold, italic)
 
-		if info in __saved_fonts:
-			return __saved_fonts[info]
+		if info in Text.__saved_fonts:
+			return Text.__saved_fonts[info]
 
-		__saved_fonts[info] = pg.font.SysFont(name, size, bold, italic)
+		Text.__saved_fonts[info] = pg.font.SysFont(name, size, bold, italic)
 
-		return __saved_fonts[info]
+		return Text.__saved_fonts[info]
 
 	
 	def __init__(self, 
@@ -33,11 +36,28 @@ class Text:
 			  color: tuple[int, int, int] = (255, 255, 255)) -> None:
 		
 		self.string = string
-		self.font = font if isinstance(font, pg.font.Font) else Text.get_font(*font)
 		self.color = color
+		
+		self.font = font if isinstance(font, pg.font.Font) else Text.get_font(*font)
 
 		self.surface = self.font.render(self.string, False, self.color)
 		self.rect = self.surface.get_rect()
 
+
+	def center_by_index(self, index: int, position: tuple[int, int]) -> None:
+
+		left: int = self.font.size(self.string[:index])[0]
+		char: int = self.font.size(self.string[index])[0]
+
+		self.rect.x = position[0]
+		self.rect.centery = position[1]
+
+		self.rect.x -= left + char // 2
+
+
 	def draw(self, surface: pg.Surface) -> None:
 		surface.blit(self.surface, self.rect)
+
+
+	def __str__(self) -> str:
+		return f"<Text: '{self.string}'; {self.rect}>"
